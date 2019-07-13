c819015e08248bbe1e22aa45cdcee48baf0fe919
swindl3 <swindlemaxxx@gmail.com>
2019-07-14 03:34:57 +0700

    Add Bootstrap

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = (props) => {
     const { owner, email } = props
-    return (<div >Powered By : {owner} || Contact Me : {email} </div>)
+    return (
+        <div className="container-fluid">
+            <hr />
+            <div className="text-center title text-uppercase">
+                <small>
+                    <span className="text-danger title">Powered By : {owner}</span> | <span className="text-muted">Contact Me : {email}</span>
+                </small>
+            </div>
+        </div>
+    )
 }
 
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -8,18 +8,18 @@ class Header extends Component {
         super(props)
         this.state = { date: new Date() }
         this.interval = 1000
-       
+
 
     }
-    componentDidMount(){
-        this.timerID =  setInterval(() => {
+    componentDidMount() {
+        this.timerID = setInterval(() => {
             this.tictok()
         }, this.interval);
     }
-    componentDidUpdate(){
-        
+    componentDidUpdate() {
+
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.timerID)
     }
 
@@ -28,8 +28,15 @@ class Header extends Component {
     }
     render() {
         return (
-            <div>
-                <h1>{this.state.date.toLocaleTimeString()}</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-10 title text-left">
+                        <h1 className="text-info"> <img style={{height:150,width:100}}src="/images/logo/logo.png" alt="   "/>  Face Attendance System (ระบบประทับเวลาด้วยใบหน้า)</h1>
+                    </div>
+                    <div className="col-md-2 title text-right">
+                        <h5 className="text-muted mt-4">{this.state.date.toLocaleTimeString()}</h5>
+                    </div>
+                </div>
             </div>
         )
     }
