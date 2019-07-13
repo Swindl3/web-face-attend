9b650a8a77de66c903650975a3c289db28975008
swindl3 <swindlemaxxx@gmail.com>
2019-07-14 00:51:49 +0700

    Update Header.js
    Add Time To Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Header = () => {
-    return <h1>This is Header</h1>
+// const Header = () => {
+//     return <h1>This is Header</h1>
+// }
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { date: new Date() }
+        this.interval = 1000
+       
+
+    }
+    componentDidMount(){
+        this.timerID =  setInterval(() => {
+            this.tictok()
+        }, this.interval);
+    }
+    componentDidUpdate(){
+        
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+
+    tictok() {
+        this.setState({ date: new Date() });
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.date.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
 }
-
-
-export default Header ;
+export default Header;
