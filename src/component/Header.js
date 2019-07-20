23b0ff2e4b6503a8d45d2fb69fa8cb43caaf1fd9
pattanna <pattanan.la@rmuti.ac.th>
2019-07-21 02:05:22 +0700

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -30,7 +30,7 @@ class Header extends Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-8 title text-left">
-                        <h2 className="text-info"> <img className="mr-5" style={{ height: 120, width: 80 }} src="/images/logo/logo.png" alt="   " />  Face Attendance System (ระบบประทับเวลาด้วยใบหน้า)</h2>
+                        <h2 className="text-info"> <img className="mr-5" style={{ height: 120, width: 80 }} src="/images/logo/logo.png" alt="   " />  Face Attendance System (ระบบประทับเวลาด้วยใบหน้า)aaaa</h2>
                     </div>
                     <div className="col-md-4 title text-right">
                         <h5 className="text-muted mt-4">{this.state.date.toLocaleTimeString()}</h5>
